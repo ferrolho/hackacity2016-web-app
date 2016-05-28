84c7fc9b8054af136ecebb1e6054f039c0aefb07
ferrolho <henriqueferrolho@gmail.com>
2016-05-28 03:37:08 +0100

    Adds Chart.js

--- resources/views/graphs.blade.php
+++ resources/views/graphs.blade.php
@@ -7,7 +7,60 @@
 
             <h2>Hello, World!</h2>
 
+            <div class="col-xs-4">
+                <div id="dataChartContainer">
+                    <canvas id="dataChart" width="400" height="400"></canvas>
+                </div>
+            </div>
+
         </div>
     </div>
 
 @endsection
+
+@push('scripts')
+<script>
+
+    var ctx = $("#dataChart");
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 5,
+                    pointHitRadius: 10,
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+</script>
+@endpush
